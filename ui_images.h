63e47a1fd70a8b2d83bd08c103d13e0b48c5d8fe
nicolasbrailo <nicolasbrailo@gmail.com>
2012-10-24 14:11:29 +0200

    Refactor

--- ui_images.h
+++ ui_images.h
@@ -2,29 +2,35 @@
 #define INCL_UI_IMAGES_H
 
 
+#include "gtk_helper/general.h"
+
+class Gtk_UI_Image : Gtk_Helper::Gtk_Object
+{
+    protected:
+        GtkWidget *img;
+    public:
+        operator GtkWidget* () { return this->img; }
+        Gtk_UI_Image() {}
+};
+
+
 #include <gtk/gtk.h>
 
 
 #include <string>
 using std::string;
-class Gtk_Image_From_File
+class Gtk_Image_From_File : public Gtk_UI_Image 
 {
-    GtkWidget *img;
     public:
-    operator GtkWidget* () { return this->img; }
-
     Gtk_Image_From_File(const string& fname)
     {
         this->img = gtk_image_new_from_file(fname.c_str());
     }
 };
 
-class Gtk_Image_From_PNG_Buff
+class Gtk_Image_From_PNG_Buff : public Gtk_UI_Image 
 {
-    GtkWidget *img;
     public:
-    operator GtkWidget* () { return this->img; }
-
     Gtk_Image_From_PNG_Buff(unsigned len, const void* buf)
     {
         auto pb_loader = gdk_pixbuf_loader_new_with_type("png", NULL);
@@ -37,12 +43,9 @@ class Gtk_Image_From_PNG_Buff
 };
 
 
-class Gtk_Image_From_Pixbuf_Buffer
+class Gtk_Image_From_Pixbuf_Buffer : public Gtk_UI_Image 
 {
-    GtkWidget *img;
     public:
-    operator GtkWidget* () { return this->img; }
-
     Gtk_Image_From_Pixbuf_Buffer(unsigned len, const void* buf)
     {
         len = len;
