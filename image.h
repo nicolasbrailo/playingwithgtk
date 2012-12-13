e346c1626b714f575626cc05ee1ec5ac893b2205
Nicolas Brailovsky <nicolasbrailo@gmail.com>
2012-12-13 22:13:08 +0100

    Preventing tile deletion while pending  updated

--- image.h
+++ image.h
@@ -7,6 +7,8 @@
 class Image : public Gtk_Helper::Image
 {
     const std::string path;
+    // TODO: Add updated stuff from class below so we dont
+    // try to delete this obj if theres a thread pending to upd it
 
     public:
         Image(const std::string &deferred_path, const std::string &temp_path)
@@ -27,16 +29,28 @@ class Image : public Gtk_Helper::Image
 
 class Image_From_File : public Gtk_Helper::Image
 {
+    bool updated;
+
     public:
         Image_From_File(const std::string &temp_path)
-            : Gtk_Helper::Image(temp_path)
+            : Gtk_Helper::Image(temp_path), updated(false)
         {}
+
+        ~Image_From_File()
+        {
+            if (not updated)
+                cout << "Someone is trying to delete me but there's i'm being updated by another thread!" << endl;
+
+            while (not updated)
+                /* lock deleting thread till done */;
+        }
 
         void update(const std::string &path)
         {
             Global_UI_Guard ui_guard;
             this->set_from_file(path);
             this->draw();
+            updated = true;
         }
 };
 
